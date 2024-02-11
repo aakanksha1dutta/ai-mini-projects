471c0f9c626f8829eb38012ed08cecfa20b9228c
aabpandit <apandit@u.rochester.edu>
2024-02-10 23:25:32 -0500

    added actions

--- hw2/Puzzle.py
+++ hw2/Puzzle.py
@@ -24,8 +24,19 @@ class Puzzle:
         if a=='U' and s.blank_x>0:
             #swap
             s_arr[self.size * (s.blank_x) + s.blank_y], s_arr[self.size * (s.blank_x-1) + s.blank_y] = s_arr[self.size * (s.blank_x-1) + s.blank_y], s_arr[self.size * (s.blank_x) + s.blank_y]
-            return State(np.array(s_arr), level=s.level+1)
-
+            return State(np.array(s_arr), level=s.level+1, parent=s)
+        
+        if a=='D' and s.blank_x>(size-1):
+            s_arr[self.size * (s.blank_x) + s.blank_y], s_arr[self.size * (s.blank_x+1) + s.blank_y] = s_arr[self.size * (s.blank_x+1) + s.blank_y], s_arr[self.size * (s.blank_x) + s.blank_y]
+            return State(np.array(s_arr), level=s.level-1, parent=s)
+        
+        if a=='L' and s.blank_y>0:
+            s_arr[self.size * (s.blank_x) + s.blank_y], s_arr[self.size * (s.blank_x) + (s.blank_y-1)] = s_arr[self.size * (s.blank_x) + (s.blank_y-1)], s_arr[self.size * (s.blank_x) + s.blank_y]
+            return State(np.array(s_arr), level=s.level, parent=s)
+        
+        if a=='R' and s.blank_y<(size-1):
+            s_arr[self.size * (s.blank_x) + s.blank_y], s_arr[self.size * (s.blank_x) + (s.blank_y+1)] = s_arr[self.size * (s.blank_x) + (s.blank_y+1)], s_arr[self.size * (s.blank_x) + s.blank_y]
+            return State(np.array(s_arr), level=s.level, parent=s)
 
         return None
         
