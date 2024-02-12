712072e59fd77eb0a9803f72c2be41becfa70126
aakanksha1dutta <aakanksha1dutta@gmail.com>
2024-02-11 19:57:46 -0500

    integrated with std in

--- hw2/solver.py
+++ hw2/solver.py
@@ -8,20 +8,30 @@ class Solver:
         return 0
     
     def main():
-        size = sys.stdin.readline().strip('\n')
-        for line in sys.stdin.readlines():
-            print(line)
+        size = sys.stdin.readline().rstrip()
+        input_arr = []
+        print("Taking in input....")
+        print()
+        print(f"The size of the array is {size}*{size}:")
+        print("Your input:")
 
-        '''arr = np.array([['1','2', '5'],
-                       ['3', '4', '8'],
-                       ['6','7','0']])
+        for line in sys.stdin.readlines():
+            l = line.rstrip()
+            print(l)
+            input_arr.append(l.split(' '))
+        
+        print()
+        print("The fastest moves to the goal is:")
+        
+
+        arr = np.array(input_arr)
         initial_state = State(arr)
         puzzle = Puzzle(initial_state, size=3)  
         path = puzzle.BFS()
         while len(path)!=0:
-            print(path.pop(), sep=" ")'''
+            print(path.pop(), sep=" ")
 
-    if __name__ == main:
+    if __name__ == "__main__":
         main() 
 
     
