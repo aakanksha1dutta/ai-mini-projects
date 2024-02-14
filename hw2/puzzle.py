c35ca9b52bccfe43bac1c5d5d1e68215f9bf940d
aakanksha1dutta <aakanksha1dutta@gmail.com>
2024-02-14 14:16:30 -0500

    aligned with project reqs

--- hw2/puzzle.py
+++ hw2/puzzle.py
@@ -7,18 +7,18 @@ class Puzzle:
         method = args[0].lstrip('--')
         size = int(sys.stdin.readline().rstrip()) #remove the \n character using rstrip
         input_arr = []
+        '''
         print("Taking in input....")
         print()
         print(f"The size of the array is {size}*{size}:")
         print("Your input:")
+        '''
 
         for line in sys.stdin.readlines():
             l = line.rstrip()
-            print(l)
+            #print(l)
             input_arr.append(l.split(' ')) #appends a row of elements
         
-        print()
-        print("The fastest moves to the goal is:")
         
 
         arr = np.array(input_arr)
