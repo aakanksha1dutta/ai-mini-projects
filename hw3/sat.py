ac233936e1ff67209e28a836a80c7af558855f9b
aakanksha1dutta <aakanksha1dutta@gmail.com>
2024-03-08 07:59:30 -0500

    added new early termination/entailment  methods

--- hw3/sat.py
+++ hw3/sat.py
@@ -4,25 +4,34 @@ disjunction = lambda *args: any(args)
 
 
 
-def find_pure(clauses):
+
+def find_pure(clauses, assignment):
     pure = {}
-    reached = []
+    deleted = []
     for clause in clauses:
         for lit in clause:
             if lit[0]=='~':
-                if lit[1:] in pure.keys():
-                    if pure[lit[1:]]!=False:
-                        del pure[lit[1:]]
-                elif lit[1:] not in reached:
-                    pure[lit[1:]]=False
-                    reached.append(lit)
+                c = lit[1:]
+                if c in assignment:
+                    continue
+                
+                elif c in pure:
+                    if pure[c]!=False:
+                        del pure[c]
+                        deleted.append(c)
+
+                elif c not in deleted:
+                    pure[c]=False
+                    
             else:
-                if lit in pure.keys():
+                if lit in assignment:
+                    continue
+                elif lit in pure:
                     if pure[lit]!=True:
                         del pure[lit]
-                elif lit not in reached:
+                        deleted.append(lit)
+                elif lit not in deleted:
                     pure[lit]=True
-                    reached.append(lit)
     
     #add the clause index where index = find(pure)
     '''listOfRemovals = []
@@ -31,6 +40,18 @@ def find_pure(clauses):
     
     return pure
 
+def isSat(clauses, assignment):
+    for c in clauses:
+        clause = []
+        for literal in c:
+            if literal[0]=='~':
+                clause.append(not assignment[literal[1:]])
+            else: 
+                clause.append(assignment[literal])             
+        #if any disjunction is False, the entire CNF expression will be false
+        if disjunction(*clause) is False:
+            return False
+    return True
 
 def unit_heuristic(clauses, assignment):
     clauses2 = clauses.copy()
@@ -59,15 +80,10 @@ def unit_heuristic(clauses, assignment):
                     
     return assignment, updated_clauses
 
-
-
-#extract literals, clauses and pure literals
-
+#extract literals, clauses
 def extract(lines):
     literals = set()
-    pure = dict() #assignments to pure
     clauses = []
-    clause_idx = 0
     
     for line in lines:
     #each clause is a disjunction in cnf form
@@ -79,56 +95,117 @@ def extract(lines):
 
     return list(literals), clauses
 
-#given an assignment, check if assignment returns True for the clauses
-def isSat(clauses, assignment):
-    for c in clauses:
-        clause = []
-        for literal in c:
-            if literal[0]=='~':
-                clause.append(not assignment[literal[1:]])
-            else: 
-                clause.append(assignment[literal])             
-        #if any disjunction is False, the entire CNF expression will be false
-        if disjunction(*clause) is False:
-            return False
+def allLiteralsinAssign(clause, assignment):
+    withoutsigns = []
+    for lit in clause:
+        if lit[0]=='~':
+            if lit[1:] not in assignment:
+                return False
+        else:
+            if lit not in assignment:
+                return False
     return True
+       
+#early termination
+#given some clauses and incomplete assignments, is it enough to conclude true or false?
+#if yes, return the assignment. if no, None.
+def isClauseTrue(clauses, assignment):
+    if len(clauses)==0 or len(assignment)==0:
+        return None
+    
+    for clause in clauses:
+        clause_isTrue = False
+        
+        for literal in clause:
+            if literal[0]=='~':
+                c = literal[1:]
+                if c in assignment and assignment[c]==False:
+                    clause_isTrue = True
+                    break
+            else:
+                if literal in assignment and assignment[literal]==True:
+                    clause_isTrue=True
+                    break
+                
+        if clause_isTrue is False:
+            return None
+    return assignment
+
+def isAnyClauseFalse(clauses, assignment):
+    if len(clauses)==0 :
+        return False
+    if len(assignment)==0:
+        return False
+    
+    #for each clause
+    for clause in clauses:
+        truth_val = []
+        #check if all literals in clause are in assignment
+        if allLiteralsinAssign(clause, assignment):
+            for literal in clause:
+                if literal[0]=='~':
+                    truth_val.append(not assignment[literal[1:]])
+                else: 
+                    truth_val.append(assignment[literal])             
+        #if any disjunction is False, the entire CNF expression will be false
+            if disjunction(*truth_val) is False:
+                return True #clause is False
+    return False #cannot determine
+
+                    
 
 
-def DPLL(clauses, literals, model,length):
+def DPLL(clauses, literals, model,length,doUnit=True, doPure=True):
+
     if len(literals)==0: #all assigned
         #find if model is satisfiable with current clauses #satisfiable should return True or False
         if isSat(clauses, model):
             return model
         else:
-            return None 
+            return None
+   
+    else:
+        #else, does the assignment entail True?
+        entailTrue = isClauseTrue(clauses, model)
+        if entailTrue:
+            #assign the unassigned literals, here literals, to True
+            return  {**model, **(dict.fromkeys(literals, True))}
+
+        entailFalse = isAnyClauseFalse(clauses,model)
+        if entailFalse:
+            return None
+        
+
+    #if doUnit only before recursion or first iteration
+    if doUnit and model=={}:
+        unit_assign, updated_clauses = unit_heuristic(clauses, {})
+        if unit_assign is None:
+            return None
+        model.update(unit_assign)
+        clauses = updated_clauses
     
-    unit_assign, updated_clauses = unit_heuristic(clauses, model)
-    if unit_assign is None:
-        return None
-    
-    pure = find_pure(updated_clauses)
-
-    #print(unit_assign)
-    d3 = {}
-    d3.update(pure)
-    #print("after pure", d3)
-    d3.update(unit_assign)
-    #print("after unit", d3)
-    d3.update(model)
-    #print("after model", d3)
+    #only check pure for first iteration
+    if doPure and model=={}:
+        pure = find_pure(clauses, model)
+        model.update(pure)
 
 
-    if len(d3)==length:
-        if isSat(clauses,d3):
-            return d3
+
+    if len(model)==length:
+        #print("STUCK AT SAT")
+        #print()
+        if isSat(clauses,model):
+            return model
         else:
             return None
 
-    rest = [i for i in literals if i not in d3.keys()] #unassigned_keys
+    rest = [i for i in literals if i not in model.keys()]     #unassigned_keys
     first = rest.pop()
 
+
     
-    return DPLL(updated_clauses, rest, {**d3, **{first:True}}, length) or DPLL(updated_clauses, rest, {**d3, **{first:False}}, length)
+    return DPLL(clauses, rest, {**model, **{first:True}}, length) or DPLL(clauses, rest, {**model, **{first:False}}, length)
+
 
 
 
@@ -136,10 +213,16 @@ def DPLL(clauses, literals, model,length):
 ######### main
 
 def main():
+
     inputLines = sys.stdin.read().splitlines()
     literals, clauses = extract(inputLines)
     length = len(literals)
-    ans = DPLL(clauses,literals, {}, length)
+
+
+    doUnit = '--nounit' not in sys.argv
+    doPure = '--nopure' not in sys.argv
+
+    ans = DPLL(clauses,literals, {}, length, doUnit, doPure)
     if ans:
         print("satisfiable", end = " ")
         for key in ans.keys():
